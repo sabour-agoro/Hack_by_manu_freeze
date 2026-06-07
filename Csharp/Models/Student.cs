f72b940638e92208465ef0a874b9425074380965
sabour-agoro <abdoulsabouragoro@gmail.com>
2026-06-07 18:24:52 +0200

    Ajout des modèles AcademicYear, Classe et Student en C#

---
+++ Csharp/Models/Student.cs
@@ -0,0 +1,15 @@
+namespace Csharp.Models;
+public enum Gender
+{
+    Feminin,
+    Masculin
+}
+public class Students
+{
+    public int Id {get; set;}
+    public String? FirstName { get; set;}
+    public String? LastName {get; set;}
+    public DateTime BirthDate {get; set;}
+    public Gender gender {get; set;}
+    
+}
